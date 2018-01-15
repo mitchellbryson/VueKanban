9c91ff4865ba815a6448887a2a6b20c94fc798fd
Mitchell Bryson <mitchell@workroll.com>
2018-01-15 14:26:13 +0000

    add kanban board component

---
+++ src/components/kanban/KanbanBoard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KanbanBoard',
+  data () {
+    return {
+      msg: "Let's Kanban!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
