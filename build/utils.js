85cdd967928badd36072c852cb1cfbadeccf085e
Mitchell Bryson <mitchell@workroll.com>
2018-01-15 14:30:59 +0000

    add bootstrap and stylesheets

--- build/utils.js
+++ build/utils.js
@@ -60,7 +60,10 @@ exports.cssLoaders = function (options) {
     postcss: generateLoaders(),
     less: generateLoaders('less'),
     sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
+    scss: generateLoaders('sass', {
+      includePaths: ['./src/styles'],
+      data: "@import 'common';"
+    }),
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
   }
